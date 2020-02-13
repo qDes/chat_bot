a627ebbabc7bc68798837508e118fd90653a6780
Andrei K <1____1@bk.ru>
2020-02-13 19:27:18 +0300

    add dialogflow

--- vk_bot.py
+++ vk_bot.py
@@ -1,21 +1,38 @@
 import os
 import vk_api
+import random
+import uuid
 
 from dotenv import load_dotenv
+from dialog import fetch_dialogflow_answer
 from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+def echo(event, vk_api, project_id,
+         session_id, language_code):
+    reply, fallback = fetch_dialogflow_answer(project_id, session_id,
+                                              event.text,
+                                              language_code)
+    if not fallback:
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message=reply,
+            random_id=random.randint(1, 1000)
+        )
+
 
 if __name__ == "__main__":
     load_dotenv()
+    google_project_id = os.environ["GOOGLE_PROJECT_ID"]
+    google_session_id = str(uuid.uuid4())
+    language_code = "ru-RU"
+
     vk_token = os.environ["VK_TOKEN"]
     vk_session = vk_api.VkApi(token=vk_token)
-
+    vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-            if event.to_me:
-                print('Для меня от: ', event.user_id)
-            else:
-                print('От меня для: ', event.user_id)
-            print('Текст:', event.text)
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api, google_project_id,
+                 google_session_id, language_code)
