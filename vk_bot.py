a90dfe671866e260acc2d6e3474813151705f970
Andrei K <1____1@bk.ru>
2020-02-14 13:51:24 +0300

    add telegram log handler

--- vk_bot.py
+++ vk_bot.py
@@ -1,12 +1,15 @@
 import os
+import logging
 import vk_api
 import random
 import uuid
 
 from dotenv import load_dotenv
 from dialog import fetch_dialogflow_answer
+from log_tools import TelegramLogsHandler
 from vk_api.longpoll import VkLongPoll, VkEventType
 
+logger = logging.getLogger("chat_bot")
 
 def echo(event, vk_api, project_id,
          session_id, language_code):
@@ -19,16 +22,22 @@ def echo(event, vk_api, project_id,
             message=reply,
             random_id=random.randint(1, 1000)
         )
+    else:
+        logger.info("Бот не может ответить пользователю.")
 
 
 if __name__ == "__main__":
     load_dotenv()
+    tg_token = os.environ["TG_TOKEN"]
+    chat_id = os.environ['TELEGRAM_CHAT_ID']
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "google-credentials.json"
     google_project_id = os.environ["GOOGLE_PROJECT_ID"]
     google_session_id = str(uuid.uuid4())
     language_code = "ru-RU"
-
     vk_token = os.environ["VK_TOKEN"]
+    logger.setLevel(logging.INFO)
+    logger.addHandler(TelegramLogsHandler(tg_token, chat_id))
+    logger.info("start vk chat bot")
     vk_session = vk_api.VkApi(token=vk_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
